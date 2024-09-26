a632119f3ec991d963c2a7e75755b60f6293e39e
Albin <albinanto06@gmail.com>
2024-09-26 17:42:10 +0530

    otp to 4

--- src/app/dragndrop/dummyData.js
+++ src/app/dragndrop/dummyData.js
@@ -1,38 +1,38 @@
-// dummyData.js
-const dummyData = [
-    {
-      id: 1,
-      playList_image: "https://via.placeholder.com/100",
-      playList_heading: "Playlist 1",
-      category: "Category 1",
-      video: [
-        {
-          video_heading: "Video 1",
-          video_link: "https://example.com/video1",
-        },
-        {
-          video_heading: "Video 2",
-          video_link: "https://example.com/video2",
-        },
-      ],
-    },
-    {
-      id: 2,
-      playList_image: "https://via.placeholder.com/100",
-      playList_heading: "Playlist 2",
-      category: "Category 2",
-      video: [
-        {
-          video_heading: "Video 1",
-          video_link: "https://example.com/video1",
-        },
-        {
-          video_heading: "Video 2",
-          video_link: "https://example.com/video2",
-        },
-      ],
-    },
-  ];
+// // dummyData.js
+// const dummyData = [
+//     {
+//       id: 1,
+//       playList_image: "https://via.placeholder.com/100",
+//       playList_heading: "Playlist 1",
+//       category: "Category 1",
+//       video: [
+//         {
+//           video_heading: "Video 1",
+//           video_link: "https://example.com/video1",
+//         },
+//         {
+//           video_heading: "Video 2",
+//           video_link: "https://example.com/video2",
+//         },
+//       ],
+//     },
+//     {
+//       id: 2,
+//       playList_image: "https://via.placeholder.com/100",
+//       playList_heading: "Playlist 2",
+//       category: "Category 2",
+//       video: [
+//         {
+//           video_heading: "Video 1",
+//           video_link: "https://example.com/video1",
+//         },
+//         {
+//           video_heading: "Video 2",
+//           video_link: "https://example.com/video2",
+//         },
+//       ],
+//     },
+//   ];
   
-  export default dummyData;
+//   export default dummyData;
   
